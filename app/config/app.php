93a51fc53d2971df02fa00df11e835fb11cea1bb
Jim Hill <ukjimbobbery@gmail.com>
2014-05-14 07:26:28 +0100

    Added Rocketeer config to app/config/app.php

--- app/config/app.php
+++ app/config/app.php
@@ -119,6 +119,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Rocketeer\RocketeerServiceProvider',
 
 	),
 
@@ -185,6 +186,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Rocketeer' 	  => 'Rocketeer\Facades\Rocketeer',
 
 	),
 
