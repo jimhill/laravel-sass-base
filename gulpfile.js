c59160058bbb7ad4ab80b7cc8979c8cce2bda84c
Jim Hill <ukjimbobbery@gmail.com>
2014-05-14 13:31:46 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,14 @@ var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var gzip = require("gulp-gzip");
-var awspublish = require('gulp-awspublish');
 
 // Variables
-var version = '0.0.1';
 var srcSassDir = './app/assets/src/sass';
 var srcCssDir = './app/assets/src/css';
 var srcJsDir = './app/assets/src/js';
-var distDir = './app/assets/dist/' + version;
-var distCssDir = './app/assets/dist/' + version + '/css';
-var distJsDir = './app/assets/dist/' + version + '/js';
-
-// Load config
-var config = JSON.parse(fs.readFileSync('./gulp-config.json', 'utf8'));
+var distDir = './app/assets/dist/';
+var distCssDir = './app/assets/dist/css';
+var distJsDir = './app/assets/dist/js';
 
 // Development Sass
 gulp.task('dev-sass', function() {
@@ -61,25 +56,6 @@ gulp.task('build-js', function() {
         .pipe(rename('master.min.js'))
         .pipe(gulp.dest(distJsDir))
         .pipe(notify('JS compiled and compressed for distribution'));
-});
-
-// Publish assets to S3
-gulp.task('publish', function() {
-
-    var publisher = awspublish.create({
-        key: config.awsKey,
-        secret: config.awsSecret,
-        bucket: config.awsBucket
-    });
-
-    var headers = {
-        'Content-Encoding': 'gzip'
-    };
-
-    return gulp.src(distDir + '/css/*')
-        .pipe(publisher.publish(headers))
-        .pipe(awspublish.reporter())
-        .pipe(notify('Pushed to AWS'));
 });
 
 // Build tasks
