9764433b9a6f0411d642cf88b36146cb7858a4ba
Jim Hill <studio@wiseguydigital.com>
2014-05-14 16:34:56 +0100

    Added AWS as local app.php config

---
+++ app/config/local/app.php
@@ -0,0 +1,42 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Application Debug Mode
+	|--------------------------------------------------------------------------
+	|
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. If disabled, a simple generic error page is shown.
+	|
+	*/
+
+	'debug' => true,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Providers
+	|--------------------------------------------------------------------------
+	|
+	| These are appended to the main app.php providers
+	|
+	*/
+	'providers' => append_config(array(
+		'Aws\Laravel\AwsServiceProvider'
+	)),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Aliases
+	|--------------------------------------------------------------------------
+	|
+	| These are appended to the main app.php aliases
+	|
+	*/
+	'aliases' => append_config(array(
+		'AWS' => 'Aws\Laravel\AwsFacade',
+	)),
+
+);
