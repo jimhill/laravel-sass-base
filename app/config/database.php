e55f79521a40d9cde6e4bdbc2b3e185f7f8966d8
Jim Hill <studio@wiseguydigital.com>
2014-07-01 17:08:01 +0100

    Updated database config to use env vars

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => $_SERVER['DB_HOST'],
+			'database'  => $_SERVER['DB_DATABASE'],
+			'username'  => $_SERVER['DB_USERNAME'],
+			'password'  => $_SERVER['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
